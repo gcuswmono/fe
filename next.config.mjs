4219b6e2166877143d3ed0897d51e26d118ff092
mango0422_mac <syj0422@gachon.ac.kr>
2024-07-07 01:56:08 +0900

    add out

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     basePath: process.env.NODE_ENV === 'production' ? '/mono_fe' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/mono_fe/' : '',
+    output: 'export',
 };
 
 export default nextConfig;
